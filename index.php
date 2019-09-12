fb8af631d3ca137833d6085721f3a6c6793e2815
pranai kongphat <pk398@afsconnect1.njit.edu>
2019-09-12 11:11:41 -0400

    my first commit

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-echo "Hello World";
+echo "Hello, is it me you're looking for?";
 ?>
+
+<!DOCTYPE html>
+<html>
+<body background="http://puzzleup.net/nothing.gif">
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>Nothing to see here</h1>
+
+</body>
+</html>
