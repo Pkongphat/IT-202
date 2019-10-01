e3cad4dcf31ce01fb944cbcb68606d12e4862197
pranai kongphat <pk398@afsconnect1.njit.edu>
2019-10-01 10:51:29 -0400

    10/1/19 jscript

--- jsSample.scratch.php
+++ jsSample.scratch.php
@@ -2,7 +2,26 @@
 <head>
 <!--css and js here -->
 <script>
-alert("Hello world");
+
+//alert("Hello world");
+
+var myVariable;
+let myOtherVariable;
+
+//myVariable = prompt("What's your name?");
+//alert("Hello, " , myVariable);
+
+let myNum = 0;
+for(let i = 0; i < 10; i++){
+
+	myNum += 0.1;
+
+}
+
+//alert("My Num is 1: " + (myNum== 1) + "\nMy Num: " + myNum);
+console.log("My Num is 1: " + (myNum== 1) + "\nMy Num: " + myNum);
+
+
 </script>
 </head>
 
