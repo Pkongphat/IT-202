ab3a7e01b9273906c233557f4828f13951743328
pranai kongphat <pk398@afsconnect1.njit.edu>
2019-10-31 17:45:15 -0400

    10/31/19 Project

--- Project/initGDB.php
+++ Project/initGDB.php
@@ -1,24 +1,53 @@
-create table if not exists `TestUsers`(
-	`id` int auto_increment not null,
-	`username` varchar(30) not null unique,
-	`pin` int default 0,
-	PRIMARY KEY (`id`)
-) CHARACTER SET utf8 COLLATE utf8_general_ci
-
-$stmt = $db->prepare($query);
-
-$r = $stmt->execute();
-//just to see the value (should be 1)
-echo "<br>" . $r . "<br>";
-
-$user = "Tom";
-$pin = 1234;
+<?php
+#turn error reporting on
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+//pull in config.php so we can access the variables from it
+require('config.php');
+echo "Loaded Host: " . $host;
+$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+try{
+	$db = new PDO($conn_string, $username, $password);
+	echo nl2br ("\nConnected\n");
+	$query = "create table if not exists `TestUsers`(
+		`id` int auto_increment not null,
+		`username` varchar(30) not null unique,
+		`pin` int default 0,
+		PRIMARY KEY (`id`)
+		) CHARACTER SET utf8 COLLATE utf8_general_ci";
+	$stmt = $db->prepare($query);
+	$r = $stmt->execute();
+	echo "<br>" . $r . "<br>";
+//Note backticks ` for table/column names and single-quotes ' for string value
+//hint: we don't need to specify `id` since it's auto increment (note this in the next steps)
+        $insert_query = "INSERT INTO `TestUsers`(`username`, `pin`) VALUES (:username, :pin)";
+        $stmt = $db->prepare($insert_query);
+        $newUser = "tom";
+        $newPin = 5311;
 //DB Insert query
 //Bind values
-$r = $stmt->execute(...);//hint: something is required here
+        $r = $stmt->execute(array("username"=> $newUser, ":pin"=>$newPin));//hint: something is required here
+	print_r($stmt->errorInfo());
+	echo "<br>" . ($r>0?"Insert Successful":"Insert Failed") . "<br>";
+}
+catch(Exception $e){
+	echo $e->getMessage();
+	exit("Something went wrong");
+}
+?>
 
-$select_query = "select * from `TestUsers` where username = :username";
 
-//previous connection/query prep/etc
-$result = $stmt->fetch();
-echo "<br><pre>" . var_export($result, true) . "</pre><br>";
+
+<!DOCTYPE html>
+<html>
+<body background="https://twistedsifter.files.wordpress.com/2016/03/scientist-street-fighter-game-pixel-art-animation-by-diego-sanches-11.gif?w=392&h=206">
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1></h1>
+
+</body>
+</html>
--- Project/registration.php
+++ Project/registration.php
@@ -1,7 +1,7 @@
 <?php
 function getName(){
-	if(isset($_GET['name'])){
-		echo "<p>Hello, " . $_GET['name'] . "</p>";
+	if(isset($_POST['name'])){
+		$user = $_POST['name'];
 	}
 }
 if(isset($_POST['password1'])){
@@ -13,6 +13,8 @@ if(isset($_POST['password2'])){
 if(isset($password1) && isset($password2)){
 if($password1 == $password2){
                 echo "<br><pre>" . "wellcome" . "</pre><br>";
+		insertSql($user, $password1);
+
 }else{
                 echo "<br><pre>" . "Error" . "</pre><br>";
 }
@@ -29,9 +31,12 @@ if($password1 == $password2){
 <body background = "http://www.glittergraphics.org/img/60/606408/fist-wallpaper.jpg"><?php getName();?>
 <form method="POST" action="#">
 <input name="name" type="text" placeholder="Enter your username" required/>
+<br>
 <!--new content-->
 <input name="password1" type="password" placeholder="Enter a password"required/>
+<br>
 <input name="password2" type="password" placeholder="Comfrim password"required/>
+<br>
 <!-- this is a comment -->
 </select>
 <!--end new content-->
@@ -41,3 +46,51 @@ if($password1 == $password2){
 </body>
 </html>
 
+
+
+
+
+<?php
+
+
+function insertSql($name, $password1){
+#turn error reporting on
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+//pull in config.php so we can access the variables from it
+require('config.php');
+$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+try{
+	$username = $name;
+        $db = new PDO($conn_string, $username, $password);
+        $query = "create table if not exists `Users`(
+                `id` int auto_increment not null,
+                `username` varchar(30) not null unique,
+                `pin` int default 0,
+                PRIMARY KEY (`id`)
+                ) CHARACTER SET utf8 COLLATE utf8_general_ci";
+        $stmt = $db->prepare($query);
+        $r = $stmt->execute();
+        echo "<br>" . $r . "<br>";
+//Note backticks ` for table/column names and single-quotes ' for string value
+//hint: we don't need to specify `id` since it's auto increment (note this in the next steps)
+        $insert_query = "INSERT INTO `Users`(`username`, `pin`) VALUES (:username, :pin)";
+        $stmt = $db->prepare($insert_query);
+        $newUser = $name;
+        $newPin = $password1;
+//DB Insert query
+//Bind values
+
+        $r = $stmt->execute(array("username"=> $newUser, ":pin"=>$newPin));//hint: something$
+        print_r($stmt->errorInfo());
+        echo "<br>" . ($r>0?"Insert Successful":"Insert Failed") . "<br>";
+}
+catch(Exception $e){
+        echo $e->getMessage();
+        exit("<br>Something went wrong");
+}
+
+
+}
+?>
