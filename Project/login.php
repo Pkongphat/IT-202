b13c6ae0e0e9a157d395727022f0a70d5d19ab2c
pranai kongphat <pk398@afsconnect1.njit.edu>
2019-11-01 13:33:45 -0400

    11/01/19 Project login / registration

--- Project/login.php
+++ Project/login.php
@@ -7,62 +7,67 @@ function getUser(){
                 $currentuserP = $_POST['password'];
         }
 }
-
-
-
-$users = array(
-	0 => "null",
-	1 => "t"
-	);
-$passwords = array(
-	0 => "null",
-	1 => "hi",
-	);
-
 ?>
-
-
 
 
 <html>
 <head>
-
 <h1>
-<font color="blue" style="font-weight:bold"> Login Page</font>
+<font color="blue" style="font-weight:bold">Login Page</font>
 </h1>
-
 </head>
-
-<body background= "https://i.pinimg.com/originals/36/cb/f3/36cbf3e3ac4c65224d4cb94133ea4f0a.gif"><?php getName();?>
-<p1>Works</p1>
+<body background = "https://i.pinimg.com/originals/36/cb/f3/36cbf3e3ac4c65224d4cb94133ea4f0a.gif"><?php getName();?>
 <form method="POST" action="#">
-
-<input name="username" type="text" placeholder="Enter your username"required/>
-
+<input name="username" type="text" placeholder="Enter your username" required/>
+<br>
 <!--new content-->
-
-<input name="password" type="password" placeholder="Enter your password"required/>
-
+<input name="password" type="password" placeholder="Enter a password"required/>
+<br>
 <!-- this is a comment -->
-
 </select>
-
 <!--end new content-->
-
-<input type="submit" value="submit"/>
-
+<input type="submit" value="Start"/>
 <input type="reset" value="Clear Form"/>
-
 </form>
-
 </body>
-
 </html>
 
-
 <?php
-if(isset($_POST)){
-        echo "<br><pre>" . var_export($_POST, true) . "</pre><br>";
-}
-?>
+require('config.php');
+$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+try{
+        $db = new PDO($conn_string, $username, $password);
+        $query = "create table if not exists `Users`(
+                `id` int auto_increment not null,
+                `username` varchar(30) not null unique,
+                `pin` int default 0,
+                PRIMARY KEY (`id`)
+                ) CHARACTER SET utf8 COLLATE utf8_general_ci";
+        $stmt = $db->prepare($query);
+        $r = $stmt->execute();
+        echo "<br>" . $r . "<br>";
+//Note backticks ` for table/column names and single-quotes ' for string value
+//DB Insert query
+//Bind values
 
+        print_r($stmt->errorInfo());
+}
+catch(Exception $e){
+        echo $e->getMessage();
+        exit("<br>Something went wrong");
+}
+
+$result = mysql_query('SELECT COUNT(*) FROM `table` WHERE `field` = ...');
+if (!$result) {
+    die(mysql_error());
+}
+if (mysql_result($result, 0, 0) > 0) {
+    // some data matched
+	echo "welcome";
+} else {
+    // no data matched
+	echo "Error";
+}
+
+
+?>
